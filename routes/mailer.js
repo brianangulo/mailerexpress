257b56a39a926f689b115736bd3e59d7b8862b5d
Brian Angulo <brianangulo96@gmail.com>
2021-05-24 01:19:13 -0400

    logging the req.body

--- routes/mailer.js
+++ routes/mailer.js
@@ -7,6 +7,7 @@ The information being sent from the front end must be specifically be JSON with 
  email, subject of email and message being sent otherwise a 400 code bad request will be sent back */
 router.post("/", function (req, res, next) {
   //If any of the required information is missing it will go to code 400 or 404
+  console.log(req.body); 
   if (req.body.email !== undefined && req.body.subject !== undefined && req.body.message !== undefined)
   {
     mail(req.body.email, req.body.subject, req.body.message)
