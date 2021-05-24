2e64bc2162a77d08b16fbe211eacbdbcbcc81877
Brian Angulo <brianangulo96@gmail.com>
2021-05-23 21:40:37 -0400

    Cleaning up

--- app.js
+++ app.js
@@ -4,24 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Routes files
 var mailRouter = require("./routes/mailer");
-
+//Main express app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+//Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+//The one and only mailing route! #microservice right?
 app.use('/mail', mailRouter);
 
 // catch 404 and forward to error handler
