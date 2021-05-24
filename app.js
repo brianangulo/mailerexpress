599ab2bbb2886d5596db3c33169bfa100266723d
Brian Angulo <brianangulo96@gmail.com>
2021-05-24 15:25:01 -0400

    adding dotenv

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var cors = require("cors");
 
 //Routes files
@@ -11,32 +12,32 @@ var mailRouter = require("./routes/mailer");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 //Middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 //The one and only mailing route! #microservice right?
-app.use('/mail', mailRouter);
+app.use("/mail", mailRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ env.js
@@ -0,0 +1,6 @@
+const MY_EMAIL = {
+  user: process.env.EMAIL,
+  password: process.env.EMAIL_PASSWORD,
+};
+
+module.exports = MY_EMAIL;
